7a4060b54baaa98e04963e74636219725a251046
mosic13 <monali.chauhan@gmail.com>
2016-04-09 21:44:42 +0530

    Azure webjob added.

---
+++ CoinScheduler/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading;
+
+namespace CoinScheduler
+{
+    class Program
+    {
+        static CityContext context;
+        public static void Main()
+        {
+            Timer t = new Timer(updateCoin, null, 0, 60000);
+            Console.ReadLine();
+        }
+        public static void updateCoin(object o)
+        {
+            context = new CityContext();
+            var city = context.Cities.ToList();
+            foreach (var item in city)
+            {
+                item.GoldCoins++;
+            }
+            context.SaveChanges();
+            Console.WriteLine("Coin Updated");
+            GC.Collect();
+        }
+    }
+
+
+    public class CityContext : DbContext
+    {
+        public CityContext() : base("CitySimulation")
+        {
+
+        }
+        public DbSet<City> Cities { get; set; }
+    }
+
+    [Table("City")]
+    public class City
+    {
+        public int CityId { get; set; }
+        public string CityName { get; set; }
+        public int GoldCoins { get; set; }
+        public Nullable<System.DateTime> CreateDate { get; set; }
+        public Nullable<System.DateTime> ModifyDate { get; set; }
+    }
+}
